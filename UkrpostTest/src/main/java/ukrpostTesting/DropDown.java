af810f8f26785e1d3447d46fe2920c7fe94f8ed1
xxxap <xxxap@DESKTOP-R72767A>
2018-02-13 21:31:47 +0200

    Dropdown actions

--- UkrpostTest/src/main/java/ukrpostTesting/DropDown.java
+++ UkrpostTest/src/main/java/ukrpostTesting/DropDown.java
@@ -1,8 +1,9 @@
 package ukrpostTesting;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -18,18 +19,15 @@ public class DropDown {
 	System.setProperty("webdriver.gecko.driver", "C:\\dev\\Selenium\\geckodriver.exe");
 	wd = new FirefoxDriver();
 	wd.manage().window().maximize();
+	wd.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
 		}
 	@Test (description = "This test will check condition of web site")
-	public void Loadsite () throws InterruptedException {
+	public void Loadsite () {
 	wd.get(ukrpostUrl);	
-	Thread.sleep(2000);
 	String currentUrl = wd.getCurrentUrl();
 	Assert.assertEquals(currentUrl, "http://ukrposhta.ua/");
-	wd.findElement(By.xpath("//*[@id=\'main-wrap\']/header/div/nav/ul/li[5]/span")).click();
-	Select oSelect = new Select(wd.findElement(By.xpath("//*[@id=\'main-wrap\']/header/div/nav/ul/li[5]/span")));
-	oSelect.deselectByVisibleText("Укрпошта Експрес");
-
-	Thread.sleep(1200);
+    Actions a = new Actions(wd);
+    a.moveToElement(wd.findElement(By.xpath("//*[@id=\"main-wrap\"]/header/div/nav/ul/li[1]/a"))).build().perform();
 		}
 	@Test (dependsOnMethods="Loadsite", description = "This test will check dropdown page") 
 	public void CheckElement () throws InterruptedException {
