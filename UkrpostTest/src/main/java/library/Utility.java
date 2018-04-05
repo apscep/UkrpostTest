f52f54256f15136faf74b2c9f2aa359ca2b3062c
xxxap <xxxap@DESKTOP-R72767A>
2018-04-05 21:48:24 +0300

    Properties

--- UkrpostTest/src/main/java/library/Utility.java
+++ UkrpostTest/src/main/java/library/Utility.java
@@ -1,13 +1,32 @@
 package library;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeSuite;
 
 public class Utility {
 
+	@BeforeSuite 
+	public static void setVariables ()
+	{
+		try {
+	Properties prop = new Properties();	
+	FileInputStream fis = new FileInputStream("./UkrpostTest/src/main/java/library/Variables.properties");
+	prop.load(fis);
+		  } 
+		catch (Exception e) {
+			System.out.println("Error during exception is"+e.getMessage());
+	
+	}
+	
+	}
+	
 	public static void CaptureScreenshot (WebDriver wd, String screenshotName) 
 	{
 		try {
